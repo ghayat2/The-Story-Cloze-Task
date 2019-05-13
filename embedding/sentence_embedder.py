a42b4e20ab6c0e3a193f47ab23320b1612ccd693
Arthur Deschamps <arthur.deschamps1208@hotmail.com>
2019-05-13 23:00:50 +0200

    Finish implementing Sentence embedder

--- embedding/sentence_embedder.py
+++ embedding/sentence_embedder.py
@@ -2,17 +2,30 @@ import pandas
 
 import embedding.skipthoughts as skipthoughts
 from definitions import ROOT_DIR
-from numpy import genfromtxt
+import numpy as np
 
 class SentenceEmbedder():
 
-    def encode(self, data_to_encode, output_filename):
+    def __init__(self, *args, **kwargs):
+        super(SentenceEmbedder, self).__init__(*args, **kwargs)
         model = skipthoughts.load_model()
-        encoder = skipthoughts.Encoder(model)
-        vectors = encoder.encode(data_to_encode)
-        vectors.save(ROOT_DIR + "/data/embeddings/skip_thoughts/embedded/" + output_filename)
+        self.encoder = skipthoughts.Encoder(model)
+
+    def encode(self, data_to_encode, batch_size=1):
+        return self.encoder.encode(data_to_encode, batch_size=batch_size)
 
 
-sEmb = SentenceEmbedder()
-eval_set = pandas.io.parsers.read_csv(ROOT_DIR + '/data/cloze_eval.csv').values
-sEmb.encode(eval_set, "cloze_eval_embedded")
+    """
+    Example to encode the 10 first stories of the evaluation set.
+    Don't use this approach to encode the full dataset, as it will take too long to process at once.
+    """
+    def get_evaluation_set_embeddings(self):
+        eval_set = pandas.io.parsers.read_csv(ROOT_DIR + '/data/cloze_eval.csv').values
+        embeddings = list()
+        for i in range(eval_set.shape[0]):
+            embeddings.append(self.encode(eval_set[i][1:-1]))
+            if (i == 10):
+                break
+        return embeddings
+
+SentenceEmbedder().get_evaluation_set_embeddings()
