9362071be6c04f241c50a28032a74efc6241aa9d
ghayat2 <ghayat@student.ethz.ch>
2019-05-14 17:17:42 +0200

    Backwards generation

---
+++ generate_backwards.py
@@ -0,0 +1,37 @@
+import numpy as np
+import functools
+import tensorflow as tf
+import data_utils as d
+
+CONTEXT_LENGTH = 4
+
+class BackPicker:
+
+    # Pick a random sample sentence
+    def pick(self, context, N = 1):
+        return [np.random.sample(context) for i in range(N)]
+    
+
+def augment_data(context, endings,
+                 BackPicker = None): # Augment the data
+
+    if BackPicker is not None:
+        randomSentence = BackPicker.pick(context)
+        endings = [endings[0], randomSentence]
+
+    return context, endings
+
+def get_data_iterator(sentences,
+                        augment_fn=functools.partial(augment_data),
+                        threads=5,
+                        batch_size=1,
+                        repeat_train_dataset=5):
+
+    # Create dataset from image and label paths
+    dataset = tf.data.Dataset.from_tensor_slices(sentences) \
+        .map(d.split_sentences, num_parallel_calls=threads) \
+        .map(augment_fn, num_parallel_calls=threads) \
+        .batch(batch_size, drop_remainder=True) \
+        .repeat(repeat_train_dataset)
+
+    return dataset
