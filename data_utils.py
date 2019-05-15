a4da67dbeb3b7342b94a33fa1e12ed3a40f9a21a
Arthur Deschamps <arthur.deschamps1208@hotmail.com>
2019-05-15 22:52:18 +0200

    Integrate tensorflow's ngrams feature extraction

--- data_utils.py
+++ data_utils.py
@@ -1,5 +1,5 @@
 # Data utilities
-
+import tensorflow as tf
 
 def endings(sentences):
     return [split_sentences(sentence)[1] for sentence in sentences]
@@ -8,3 +8,18 @@ def endings(sentences):
 def split_sentences(sentences):
     # Split sentences into [context], ending
     return sentences[0:CONTEXT_LENGTH], [sentences[CONTEXT_LENGTH]]
+
+
+def sentences_to_sparse_tensor(sentences):
+    separated_sentences = sentences.split(".")
+    dim1 = len(separated_sentences)
+    dim2 = 0
+    indices = []
+    values = []
+    for i in range(len(separated_sentences)):
+        words = separated_sentences[i].split()
+        dim2 = max(dim2, len(words))
+        for j in range(len(words)):
+            indices.append((i,j))
+            values.append(words[j])
+    return tf.SparseTensor(values=values, indices=indices, dense_shape=(dim1, dim2))
