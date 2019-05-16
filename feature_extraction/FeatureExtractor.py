deea328911d7bc8eaf04e35d210701d185f57029
Arthur Deschamps <arthur.deschamps1208@hotmail.com>
2019-05-16 21:58:39 +0200

    Implement first version of pronoun contrast
    Produce tensors with 0s for pronouns from the story that are also present in the ending, and 1s for all others.

--- feature_extraction/FeatureExtractor.py
+++ feature_extraction/FeatureExtractor.py
@@ -4,10 +4,11 @@ import tensorflow_transform as tft
 import tensorflow as tf
 from data_utils import sentences_to_sparse_tensor as to_sparse
 import numpy as np
+import nltk
 
 class FeatureExtractor():
 
-    def __init__(self, tf_session, story, ending1, ending2, ngram_range=(1,3)):
+    def __init__(self, tf_session, story, ending1, ending2):
         """
 
         :param tf_session: Tensorflow session
@@ -19,17 +20,38 @@ class FeatureExtractor():
         self.story = story
         self.ending1 = ending1
         self.ending2 = ending2
-        self.ngram_range = ngram_range
 
 
-    def nGramsOverlap(self, word_count=True):
+    def pronounContrast(self):
+        get_pronouns = lambda strings: list(map(
+            lambda word_and_tag: word_and_tag[0],
+            filter(
+                lambda word_and_tag: "PRP" in word_and_tag[1],
+                nltk.pos_tag(nltk.word_tokenize(strings))
+            )
+        ))
+        story_pronouns = get_pronouns(self._merged_story())
+        endings_pronouns = (get_pronouns(self.ending1), get_pronouns(self.ending2))
+        endings_pronouns_mismatch = ([], [])
+        for story_pronoun in story_pronouns:
+            for i in range(2):
+                endings_pronouns_mismatch[i].append(0 if (story_pronoun in endings_pronouns[i]) else 1)
+        return list(map(lambda mismatches: tf.constant(mismatches), endings_pronouns_mismatch))
+
+
+    def nGramsOverlap(self, ngram_range=(1,3), character_count=True):
         """
         Counts the number of overlapping n-grams between the story and the two endings.
+
+        :param ngram_range: Range of n-grams to inspect.
+        :parameter character_count: If the values of the feature vector will be 0s and 1s or 0s and the varying
+        number of characters of the matched n-grams.
+
         :return: One feature vector for each ending, having the length of the number of ngrams in the story. Each
         component of the vector is 0 if the n-gram is not in the ending. Otherwise, if word_count is True, the value
         is the amount of letters in the n-gram. If word_count is False, it is simply 1.
         """
-        story_ngrams_vec, ending1_ngrams_vec, ending2_ngrams_vec = self._nGrams()
+        story_ngrams_vec, ending1_ngrams_vec, ending2_ngrams_vec = self._nGrams(ngram_range)
         # Retrieves the ngram 1d tensor of strings from the sparse vectors.
         story_ngrams = tf.unstack(story_ngrams_vec.values, num=story_ngrams_vec.indices.shape[0])
 
@@ -51,11 +73,13 @@ class FeatureExtractor():
                 ending_ngram_overlap,
                 index,
                 tf.multiply(
+                    # Produces 1 if any ngram in the ending matches story_ngram and 0 otherwise
                     tf.cast(tf.math.reduce_any(tf.strings.regex_full_match(
                         ending_ngrams_vec.values,
                         story_ngram
                     )), dtype=tf.int32),
-                    tf.strings.length(story_ngram, unit="UTF8_CHAR") if (word_count) else tf.constant(1, tf.int32)
+                    # Returns either 1 if word_count if False or the number of characters in the matched n-gram
+                    tf.strings.length(story_ngram, unit="UTF8_CHAR") if (character_count) else tf.constant(1, tf.int32)
                 )
             )
 
@@ -66,33 +90,34 @@ class FeatureExtractor():
         return ending1_ngram_overlap, ending2_ngram_overlap
 
 
-    def _nGrams(self):
+    def _nGrams(self, ngram_range):
         """
         :return A list containing, in order: n-gram feature vector of story, n-gram feature vector of first
         ending and finally n-gram feature vector of second ending.
         """
         return (
             tft.ngrams(
-                tokens=to_sparse(reduce(lambda sen1, sen2: sen1 + sen2, self.story)),
-                ngram_range=self.ngram_range,
+                tokens=to_sparse(self._merged_story()),
+                ngram_range=ngram_range,
                 separator=" ",
                 name="story_ngram_feature_vector"
             ),
             tft.ngrams(
                 tokens=to_sparse(self.ending1),
-                ngram_range=self.ngram_range,
+                ngram_range=ngram_range,
                 separator=" ",
                 name="ending1_ngram_feature_vector"
             ),
             tft.ngrams(
                 tokens=to_sparse(self.ending2),
-                ngram_range=self.ngram_range,
+                ngram_range=ngram_range,
                 separator=" ",
                 name="ending2_ngram_feature_vector"
             )
         )
 
-
+    def _merged_story(self):
+        return reduce(lambda sen1, sen2: sen1 + " " + sen2, self.story)
 
 
 def test_ngrams():
@@ -107,3 +132,17 @@ def test_ngrams():
             sess.run(init)
             print(ending1_ngram_overlaps.eval())
             print(ending2_ngram_overlaps.eval())
+
+
+def test_pronoun_contrast():
+    with tf.Graph().as_default():
+        story = ["The man saw a boat.", "He bought the it."]
+        ending1 = "He then proceeded to sail the boat"
+        ending2 = "She then proceeded to sail the boat"
+        sess = tf.Session()
+        with sess.as_default():
+            mis1, mis2 = FeatureExtractor(sess, story, ending1, ending2).pronounContrast()
+            print(mis1.eval())
+            print(mis2.eval())
+
+test_pronoun_contrast()
