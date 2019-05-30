6a74dedaf6700d3c6f45d6af83ca5b0bfd7f35ec
Hidde L <mail.hidde@gmail.com>
2019-05-30 16:32:08 +0200

    Invert labels for sigmoid

--- losses.py
+++ losses.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 def sigmoid(labels, train_logits):
     return tf.reduce_mean(
         # loss something
-        tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.cast(labels, dtype=tf.float32), logits=train_logits)
+        tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.cast(1 - labels, dtype=tf.float32), logits=train_logits)
     )
 
 def sparse_softmax(labels, endings):
