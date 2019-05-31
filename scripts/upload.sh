5accc8b0127a8d9fe92ee503d7e711e8b423093e
Hidde L <mail.hidde@gmail.com>
2019-05-31 17:12:02 +0200

    Set back

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,3 +1,3 @@
 scp -r data lhidde@login.leonhard.ethz.ch:/cluster/home/lhidde/nlu-project2
 
-scp -r lhidde@login.leonhard.ethz.ch:/cluster/home/lhidde/nlu-project2/runs/force_reuse_impl/summaries runs/force_reuse_impl
+scp -r lhidde@login.leonhard.ethz.ch:/cluster/home/lhidde/nlu-project2/runs/force_reuse_nicer/summaries runs/force_reuse_nicer
