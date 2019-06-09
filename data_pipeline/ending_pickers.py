0b3891d78cd1764722da3e7b918786f0b407c13a
ghayat2 <ghayat@student.ethz.ch>
2019-06-09 21:45:15 +0200

    bug fix

--- data_pipeline/ending_pickers.py
+++ data_pipeline/ending_pickers.py
@@ -35,12 +35,12 @@ class BackPicker(Picker):
         return tf.gather(context, rand_index).numpy()[0].decode("utf-8")
 
 
-class PlainRandomPicker(Picker, N = 1):
+class PlainRandomPicker(Picker):
 
     def __init__(self, *args, **kwargs):
         super(PlainRandomPicker, self).__init__(*args, **kwargs)
         csv_path = f"{ROOT_DIR}/data/train_stories.csv"
         self.dataset = pd.read_csv(csv_path, delimiter=",", usecols=["sentence5"], dtype=str, encoding='utf-8').values.flatten()
 
-    def pick(self, _):
+    def pick(self, N=1):
         return np.random.choice(self.dataset)
